8f6b54bef813ca85af8ed64d2753adde4c27a2ea
Lucca <luccadelmano@gmail.com>
2023-04-08 18:31:50 -0300

    update req params

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post("/webhook", async (req, res) => {
   try {
-    const messageText = req.body.message.text;
-    const senderPhoneNumber = req.body.sender.phone_number;
+    const messageText = req.body.Body;
+    const senderPhoneNumber = req.body.From;
 
     const responseText = await processMessage(messageText);
 
