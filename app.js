a44bf674dcad298a347649caccbf8315bd10c833
Lucca Delmano <luccadelmano@gmail.com>
2023-04-10 19:34:16 -0300

    WIP: include chat gpt command

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const zapfy = require("zapfy-sdk");
+const { processMessage } = require("./chatgptstuff");
 
 const { commands } = require("./commands");
 
@@ -15,32 +16,34 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.post("/webhook", async (req, res) => {
   try {
     const { type, data } = req.body;
-    // const senderPhoneNumber = req.body.From;
 
     let responseText = "";
     const messageText = data?.message?.conversation;
 
     if (type === "NEW-MESSAGE" && messageText) {
       if (!data?.key?.fromMe) {
-        if (commands.hasOwnProperty(messageText)) {
+        const messageParts = messageText.split(" ");
+        const command = messageParts[0];
+        const content = messageParts.slice(1).join(" ");
+
+        if (commands.hasOwnProperty(command)) {
           // If the message is a predefined command and has a fixed response
-          if (commands[messageText].fixed) {
+          if (commands[command].fixed) {
             responseText =
-              typeof commands[messageText].response === "function"
-                ? commands[messageText].response()
-                : commands[messageText].response;
+              typeof commands[command].response === "function"
+                ? commands[command].response()
+                : commands[command].response;
           } else {
             // Otherwise, process the command using the GPT-3 API with context
             responseText = await processMessage(
-              messageText,
-              commands[messageText].context
+              content,
+              commands[command].context
             );
           }
+        } else {
+          // If the message is not a command, process it using the GPT-3 API
+          responseText = await processMessage(messageText);
         }
-        // else {
-        //   // If the message is not a command, process it using the GPT-3 API
-        //   responseText = await processMessage(messageText);
-        // }
       } else if (messageText === "/help") {
         responseText = commands["/help"].response;
       }
--- commands.js
+++ commands.js
@@ -14,14 +14,15 @@ const commands = {
     description: "Show this help message",
     fixed: true,
   },
-  "/command2": {
-    response: "This is the response for command2.",
-    description: "Description for command2",
-    fixed: true,
-  },
-  "/dynamic": {
-    description: "A dynamic command that goes through processMessage",
-    context: "This is some context for the dynamic command.",
+  // "/command2": {
+  //   response: "This is the response for command2.",
+  //   description: "Description for command2",
+  //   fixed: true,
+  // },
+  "/delmano-chat": {
+    description:
+      'Get a response from Chat GPT. Write /delmano-chat "your question"',
+    context: "",
   },
 };
 
